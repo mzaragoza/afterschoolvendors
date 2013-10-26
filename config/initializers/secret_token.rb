fb0b7a736babd5a383d232a62ef7a618666ed6db
Moises Zaragoza <mZaragoza@circletechfl.com>
2013-10-26 15:29:54 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Afterschoolvendors::Application.config.secret_key_base = 'b1950137c572e5c7e9bdbccc6bc1e6766358834cba00c5687fd965d1a4e4329bcba89ca295f57ae7f772f893634a91cbbb5a77577ca622370cb96ca1c0a8d9a6'
