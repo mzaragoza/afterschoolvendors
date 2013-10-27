13eaf9b752fbb9cc7ade035099d8b2dac9d9d412
Moises Zaragoza <mZaragoza@circletechfl.com>
2013-10-27 10:59:14 -0400

    update layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Afterschoolvendors::Application.routes.draw do
   # You can have the root of your site routed with "root"
   get 'ping' => 'pages#ping', as: :ping
+  match 'about_us' => 'pages#about_us', as: :about_us, via: :all
+  match 'list_with_us' => 'pages#list_with_us', as: :list_with_us, via: :all
+  match 'contact_us' => 'pages#contact_us', as: :contact_us, via: :all
+  match 'faq' => 'pages#faq', as: :faq, via: :all
   root 'pages#index'
 end
